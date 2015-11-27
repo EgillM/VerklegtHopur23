e399e9c95ef53495cb4f7bc61f5ae5cbee0b4e2f
sigurdurhelga <sigurdurhel15@ru.is>
2015-11-27 11:13:42 +0000

    basic functions

--- QTFILES/main.cpp
+++ QTFILES/main.cpp
@@ -1,4 +1,5 @@
-//#include <QCoreApplication>
+#include <QCoreApplication>
+#include "scientist.h"
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -6,58 +7,44 @@
 
 using namespace std;
 
-int currentLength(vector<string>& db){
-    ifstream dataBase("database.txt");
-    int i = 1;
-    while(getline(dataBase, i) != ""){
-      i++;
-    }
-    return i*4;
-}
-void addToList(vector<string>& db){
+void addToList(vector<scientist>& db){
     string name = "";
-    string sex = "";
-    string dateOfBirth = "";
-    string dateOfDeath = "";
-    cout << "Write the name of the scientist: ";
+    bool sex = 0;
+    string doB = "";
+    string doD = "";
+
+    cout << "Write a name for the scientist: ";
     cin >> name;
-    db.push_back(name);
-    cout << "Write 'male' if they are male and 'female' if they are female: ";
+    cout << "Write 0 for female and 1 for male: ";
     cin >> sex;
-    db.push_back(sex);
-    cout << "Wrtie the date of birth (DD/MM/YYYY): ";
-    cin >> dateOfBirth;
-    db.push_back(dateOfBirth);
-    cout << "Write the date of death (DD/MM/YYYY) or ALIVE: ";
-    cin >> dateOfDeath;
-    db.push_back(dateOfDeath);
-    CURR_PLACE += 4;
+    cout << "Write the date of birth for your scientist: ";
+    cin >> doB;
+    cout << "Write the date of death for your scientist: ";
+    cin >> doD;
 
-}
-void saveList(vector<string> db){
-    ifstream text ("database.txt");
-    vector<string> tempDB;
+    scientist newScientist = scientist(name, sex, doB, doD);
 
+    db.push_back(newScientist);
 }
 
-void listItUp(vector<string>& db){
-    for (int i = 0; i <= CURR_PLACE; i++) {
-          cout << db[i];
-    }
+void listTheList(vector<scientist>& db){
+
+    cout << db[0].returnName();
+
 }
 
 int main(int argc, char *argv[])
 {
-	//QCoreApplication a(argc, argv);
-    vector<string> db(120);
+    QCoreApplication a(argc, argv);
+    vector<scientist> db;
     int select = 25;
 	  cout << "This is commandline interface for a CS scienctist program" << endl;
     while (select != 0){
-    cout << "1. List all scientists" << endl << "2. Add new scientist" << endl << "3. Remove scientist" << endl << "4. Search by name" << endl << "0. Quit the application" << endl;
+        cout << "1. List all scientists" << endl << "2. Add new scientist" << endl << "3. Remove scientist" << endl << "4. Search by name" << endl << "0. Quit the application" << endl;
         cin >> select;
         switch(select){
             case 1:
-                listItUp(db);
+                //listItUp(db);
                 break;
             case 2:
                 addToList(db);
@@ -66,6 +53,6 @@ int main(int argc, char *argv[])
                 continue;
         }
     }
-	return 0;
-	//return a.exec();
+    //return 0;
+    return a.exec();
 }
